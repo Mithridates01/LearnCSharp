115d645dc97198d9ec53cf8abef6d1097362a225
Mithridates01 <jclucas21@gmail.com>
2017-05-28 23:13:04 -0400

    Finish crashcourse Video

--- Learning.cs
+++ Learning.cs
@@ -52,8 +52,67 @@ namespace LearningCSharp
       Console.WriteLine(totalPrice);
       const float Pi = 3.14f;
       // complier wont let constant be changed
-      // TYPE Conversion
 
+
+      // TYPE Conversion
+      // Implicit type conversion
+      // fills the 8 bit memory
+      byte b = 1; // 0000001
+      int i = b; // 000000 000000 00000 0000001
+
+      int i = 1;
+      float  f = i;
+
+      int i = 1;
+      byte b = i; // wont compiled
+
+      float f = 1.0f;
+      int i = f // chance of data lose because
+      // float takes up more bytes of space than an int
+      // do explicit Casting type conversion
+      int i = (int)f; // will work, but may lose data
+
+
+      // explicit type conversion
+      // conversion between non compatible types
+      // use convert class or Parse() method to convert between types
+      string s = "1";
+      int i  = Convert.ToInt32(s); // -> 1
+      int j = int.Parse(s);  // -> 1
+      string newString = string.Parse(j); // -> "j"
+
+      ToInt32();
+      ToInt16();
+
+
+      try
+      {
+        // this code will be monitored
+        // and if an error occures,
+        // the catch block will throw an error
+        // without crashing the application
+      }
+      catch (Exception)
+      {
+        Console.WriteLine("Warning Error")
+      }
+
+      // Operators
+        // Arithmetic Operators
+        // math same as other langs
+        // has -- & ++
+      // comparison
+
+      //  && and
+      //  || or
+      //  ! not
+      // Bitwise Operators
+      // &    a & b
+      // !    a | b
+
+      /* 
+        Like CSS comments
+      */
 
     }
   }
@@ -61,4 +120,16 @@ namespace LearningCSharp
 
 
 
-// 
+
+
+
+
+
+
+
+
+
+
+
+
+
