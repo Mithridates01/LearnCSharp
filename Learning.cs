a4840db2d9582392b104bbd7876bedf2b4231328
Mithridates01 <jclucas21@gmail.com>
2017-05-28 22:47:22 -0400

    moving onto type conversion

--- Learning.cs
+++ Learning.cs
@@ -40,7 +40,20 @@ namespace LearningCSharp
       }
       // program will throw compile time error if accessed
       // outside of scope.
-      
+      byte number = 2;
+      int count = 10;
+      float totalPrice = 20.95f;
+      char character = 'A';
+      string  firstString = "YAY!";
+      bool isWorking = true;
+      bool notWork = false;
+      Console.WriteLine(number);
+      Console.WriteLine(count);
+      Console.WriteLine(totalPrice);
+      const float Pi = 3.14f;
+      // complier wont let constant be changed
+      // TYPE Conversion
+
 
     }
   }
